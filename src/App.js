0061e91130bd5c6ceef208c6a74b150c84743efd
Yoga Kurniawan <yogaygk@gmail.com>
2022-04-30 22:36:16 +0700

    phone input update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [inputType, setInputType] = useState("text");
+  const [value, setValue] = useState("");
+  const inputTel = React.createRef();
+  const inputText = React.createRef();
+  const handleChange = (e) => {
+    const theValue = (e.target.value || '').trim();
+    if (isNaN(theValue) || theValue === '') {
+      setInputType("text");
+    } else {
+      setInputType("tel");
+    }
+    setValue(theValue);
+  };
+
+  useEffect(() => {
+    if (inputType === "tel") {
+      inputTel.current.focus();
+    } else {
+      inputText.current.focus();
+    }
+  }, [inputType]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {inputType === "tel" && (
+        <input
+          ref={inputTel}
+          name="phone input"
+          type="tel"
+          value={value}
+          onChange={handleChange}
+        />
+      )}
+      {inputType === "text" && (
+        <input
+          ref={inputText}
+          name="phone input"
+          value={value}
+          type="text"
+          onChange={handleChange}
+        />
+      )}
+    </>
   );
 }
 
